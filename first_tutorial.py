fd8dd1fd5fb2fed2a1e24e4b11bde01a429c6551
wshuts <shutswilliam@gmail.com>
2022-08-16 15:09:40 -0500

    area under curve added to scene

--- first_tutorial.py
+++ first_tutorial.py
@@ -4,4 +4,5 @@ class SecondExample(Scene):
     def construct(self):
         ax = Axes(x_range=(-3, 3), y_range=(-3, 3))
         curve = ax.plot(lambda x: (x+2)*x*(x-2), color=RED)
-        self.add(ax, curve)
+        area = ax.get_area(curve, x_range=(-2, 0))
+        self.add(ax, curve, area)
