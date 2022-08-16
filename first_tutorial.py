de9003d2d7084bd7a5227dc7cf73f7e417d902b9
wshuts <shutswilliam@gmail.com>
2022-08-16 14:40:21 -0500

    remove .gitignore

--- first_tutorial.py
+++ first_tutorial.py
@@ -3,4 +3,5 @@ from manim import *
 class SecondExample(Scene):
     def construct(self):
         ax = Axes(x_range=(-3, 3), y_range=(-3, 3))
-        self.add(ax)
+        curve = ax.plot(lambda x: (x+2)*x*(x-2), color=RED)
+        self.add(ax, curve)
