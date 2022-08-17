568bf09f2fa3eef0cf7858f055bbaddab6006159
wshuts <shutswilliam@gmail.com>
2022-08-17 16:22:58 -0500

    indicate animation (emphasis)

--- first_tutorial.py
+++ first_tutorial.py
@@ -16,4 +16,5 @@ class SquareToCircle(Scene):
         self.play(DrawBorderThenFill(green_square))
         blue_circle = Circle(color=BLUE, fill_opacity=0.5)
         self.play(ReplacementTransform(green_square, blue_circle))
+        self.play(Indicate(blue_circle))
         self.play(FadeOut(blue_circle))
