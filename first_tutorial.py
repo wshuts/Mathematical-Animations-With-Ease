867fcac434142dc1b564a9e859da83ab2dc1b2d1
wshuts <shutswilliam@gmail.com>
2022-08-17 15:28:46 -0500

    run_time for each animation

--- first_tutorial.py
+++ first_tutorial.py
@@ -6,6 +6,6 @@ class SecondExample(Scene):
         curve = ax.plot(lambda x: (x+2)*x*(x-2), color=RED)
         area = ax.get_area(curve, x_range=(-2, 0))
 
-        self.play(Create(ax), Create(curve), run_time=3)
+        self.play(Create(ax, run_time=2), Create(curve, run_time=5))
         self.play(FadeIn(area))
         self.wait(2)
