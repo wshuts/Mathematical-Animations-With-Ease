283ea34219bc54425652b05db5812a365d629a26
wshuts <shutswilliam@gmail.com>
2022-08-17 16:12:56 -0500

    fade out the mutated "green_square"

--- first_tutorial.py
+++ first_tutorial.py
@@ -16,4 +16,4 @@ class SquareToCircle(Scene):
         self.play(DrawBorderThenFill(green_square))
         blue_circle = Circle(color=BLUE, fill_opacity=0.5)
         self.play(Transform(green_square, blue_circle))
-        self.wait(2)
+        self.play(FadeOut(green_square))
