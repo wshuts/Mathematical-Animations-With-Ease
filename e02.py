06f0eacca8dc4e6ed336aa95806166415147f18e
wshuts <shutswilliam@gmail.com>
2022-08-18 00:46:37 -0500

    align_to

--- e02.py
+++ e02.py
@@ -18,3 +18,11 @@ class Positioning(Scene):
         c.move_to([-3,-2,0])
         self.add(c)
 
+        c2 = Circle(radius=0.5, color=RED, fill_opacity=0.5)
+        c3 = c2.copy().set_color(ORANGE)
+        c4 = c2.copy().set_color(YELLOW)
+        c2.align_to(s, UP)
+        c3.align_to(s, RIGHT)
+        c4.align_to(s, UP + RIGHT)
+        self.add(c2, c3, c4)
+
