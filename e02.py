58d99482247a80ab0979a0b21129cae449d5c3cf
wshuts <shutswilliam@gmail.com>
2022-08-18 14:17:50 -0500

    shift by Pixels dependency on rendering quality (-l,-m,-h)

--- e02.py
+++ e02.py
@@ -46,3 +46,7 @@ class UsefulUnits(Scene):
         for perc in range(5,51,5):
             self.add(Circle(radius=perc*Percent(X_AXIS)))
             self.add(Square(side_length=2*perc*Percent(Y_AXIS), color=YELLOW))
+
+        d = Dot()
+        d.shift(100 * Pixels * RIGHT)
+        self.add(d)
