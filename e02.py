fac76163915a54573cd9719cb131db50184aa1db
wshuts <shutswilliam@gmail.com>
2022-08-18 00:19:00 -0500

    positioning green dot with next_to

--- e02.py
+++ e02.py
@@ -4,3 +4,8 @@ class Positioning(Scene):
     def construct(self):
         plane = NumberPlane()
         self.add(plane)
+
+        red_dot = Dot(color=RED)
+        green_dot = Dot(color=GREEN)
+        green_dot.next_to(red_dot, RIGHT + UP)
+        self.add(red_dot, green_dot)
