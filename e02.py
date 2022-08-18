220af75e9856af545874aa3a3a54073cc8298648
wshuts <shutswilliam@gmail.com>
2022-08-18 14:10:09 -0500

    squares with side_length percent of y-axis

--- e02.py
+++ e02.py
@@ -45,3 +45,4 @@ class UsefulUnits(Scene):
     def construct(self):
         for perc in range(5,51,5):
             self.add(Circle(radius=perc*Percent(X_AXIS)))
+            self.add(Square(side_length=2*perc*Percent(Y_AXIS), color=YELLOW))
