9b6ded11a728e024c42f9b529711944993904245
wshuts <shutswilliam@gmail.com>
2022-08-19 14:43:10 -0500

    arrange in row x column grid with buffer to scale spacing

--- e02.py
+++ e02.py
@@ -64,3 +64,7 @@ class Grouping(Scene):
         circles = VGroup(*[Circle(radius = 0.2) for _ in range(10)])
         circles.arrange(UP, buff=0.5)
         self.add(circles)
+
+        stars = VGroup(*[Star(color=YELLOW, fill_opacity=1).scale(0.5) for _ in range(20)])
+        stars.arrange_in_grid(4, 5, buff=0.2)
+        self.add(stars)
