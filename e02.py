7e1da36c1e1493dcc230b457f3376c4864b18d96
wshuts <shutswilliam@gmail.com>
2022-08-19 14:18:02 -0500

    apply to_edge(RIGHT) to the group

--- e02.py
+++ e02.py
@@ -57,4 +57,5 @@ class Grouping(Scene):
         green_dot = Dot(color=GREEN).next_to(red_dot, RIGHT)
         blue_dot = Dot(color=BLUE).next_to(red_dot, UP)
         dot_group = VGroup(red_dot, green_dot, blue_dot)
+        dot_group.to_edge(RIGHT)
         self.add(dot_group)
