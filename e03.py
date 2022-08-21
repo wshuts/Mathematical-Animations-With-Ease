48e0b3c2f0a9947ce03468b6cec8c1cc4fca9fb1
wshuts <shutswilliam@gmail.com>
2022-08-21 15:38:43 -0500

    playing simultaneous animations with various rate functions

--- e03.py
+++ e03.py
@@ -3,7 +3,15 @@ from manim import *
 class BasicAnimations(Scene):
     def construct(self):
         polys = VGroup(
-            *[RegularPolygon(5, radius=1) for j in range(5)]
+            *[RegularPolygon(5, radius=1, fill_opacity=0.5) for j in range(5)]
         ).arrange(RIGHT)
-        self.play(Create(polys))
+        self.play(DrawBorderThenFill(polys, run_time=2))
+        self.play(
+            Rotate(polys[0], PI, rate_func=lambda t: t),
+            Rotate(polys[1], PI, rate_func=smooth),
+            Rotate(polys[2], PI, rate_func=lambda t: np.sin(PI*t)),
+            Rotate(polys[3], PI, rate_func=there_and_back),
+            Rotate(polys[4], PI, rate_func=lambda t: 1-abs(1-2*t)),
+            run_time=2
+        )
         self.wait()
