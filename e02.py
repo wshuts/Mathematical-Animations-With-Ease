6e5c3101c7768f4d8364276fcb7aa2a6f751cede
wshuts <shutswilliam@gmail.com>
2022-08-20 21:21:21 -0500

    file level config settings

--- e02.py
+++ e02.py
@@ -68,3 +68,10 @@ class Grouping(Scene):
         stars = VGroup(*[Star(color=YELLOW, fill_opacity=1).scale(0.5) for _ in range(20)])
         stars.arrange_in_grid(4, 5, buff=0.2)
         self.add(stars)
+
+config.background_color = WHITE
+
+class SimpleScene(Scene):
+    def construct(self):
+        t = Triangle(color=PURPLE, fill_opacity=0.5)
+        self.add(t)
