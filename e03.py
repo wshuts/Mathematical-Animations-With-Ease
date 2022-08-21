197ae942d14a65324b47071337dde85a3ded7cd0
wshuts <shutswilliam@gmail.com>
2022-08-21 16:10:07 -0500

    Color class  using HSL args

--- e03.py
+++ e03.py
@@ -1,9 +1,11 @@
 from manim import *
+from colour import Color
 
 class BasicAnimations(Scene):
     def construct(self):
         polys = VGroup(
-            *[RegularPolygon(5, radius=1, fill_opacity=0.5) for j in range(5)]
+            *[RegularPolygon(5, radius=1, fill_opacity=0.5,
+            color=Color(hue=j/5,saturation=1,luminance=0.5)) for j in range(5)]
         ).arrange(RIGHT)
         self.play(DrawBorderThenFill(polys, run_time=2))
         self.play(
