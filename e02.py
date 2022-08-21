79e34a386545bf867e07640e63d1f78418a76db3
wshuts <shutswilliam@gmail.com>
2022-08-20 21:30:41 -0500

    frame height, width example

--- e02.py
+++ e02.py
@@ -69,9 +69,12 @@ class Grouping(Scene):
         stars.arrange_in_grid(4, 5, buff=0.2)
         self.add(stars)
 
-config.background_color = WHITE
+config.background_color = DARK_BLUE
+config.frame_width = 16
+config.frame_height = 9
 
 class SimpleScene(Scene):
     def construct(self):
         t = Triangle(color=PURPLE, fill_opacity=0.5)
-        self.add(t)
+        np = NumberPlane(x_range=(-8,8),y_range=(-4.5,4.5))
+        self.add(t, np)
