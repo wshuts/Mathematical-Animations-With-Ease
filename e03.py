45ef1081af16f6d0ea5bad60558085be2d27c16c
wshuts <shutswilliam@gmail.com>
2022-08-21 23:28:55 -0500

    MoveToTarget animation generate copy, transform copy

--- e03.py
+++ e03.py
@@ -47,3 +47,15 @@ class AnimateProblem(Scene):
         self.add(left_square, right_square)
         self.play(left_square.animate.rotate(PI), Rotate(right_square, PI), run_time=2)
         self.wait()
+
+class AnimationMechanisms(Scene):
+    def construct(self):
+        c = Circle()
+
+        c.generate_target()
+        c.target.set_fill(color=GREEN, opacity=0.5)
+        c.target.shift(2*RIGHT + UP).scale(0.5)
+
+        self.add(c)
+        self.wait()
+        self.play(MoveToTarget(c))
