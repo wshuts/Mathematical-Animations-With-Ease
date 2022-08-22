6a0f55033ca53fd0b0a83d0b0b5360f65ff95196
wshuts <shutswilliam@gmail.com>
2022-08-21 23:14:14 -0500

    contrast animate.rotate with Rotate weird rotate behavior due to interpolation

--- e03.py
+++ e03.py
@@ -38,3 +38,12 @@ class AnimateSyntax(Scene):
         self.play(s.animate.shift(UP), c.animate.shift(DOWN))
         self.play(VGroup(s,c).animate.arrange(RIGHT, buff=1))
         self.play(c.animate(rate_func=linear).shift(RIGHT).scale(2))
+
+class AnimateProblem(Scene):
+    def construct(self):
+        left_square = Square()
+        right_square = Square()
+        VGroup(left_square, right_square).arrange(RIGHT, buff=1)
+        self.add(left_square, right_square)
+        self.play(left_square.animate.rotate(PI), Rotate(right_square, PI), run_time=2)
+        self.wait()
