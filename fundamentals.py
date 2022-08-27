98b6484260a1c3fa017dec0e31ed2946c591fe0b
wshuts <shutswilliam@gmail.com>
2022-08-27 15:07:15 -0500

    AddWait, ShiftWait for one square

--- fundamentals.py
+++ fundamentals.py
@@ -5,10 +5,23 @@ SECONDS = 10
 class AddWaitRemove(Scene):
     def construct(self):
         square = Square()
-        self.add(square)
+        for _ in range(SECONDS):
+            self.AddWait(square)
+            self.ShiftWait(square, 1)
+            #self.RemoveWait(square)
+
+    def ShiftWait(self, square, shift):
+        square.shift(RIGHT*shift)
         self.wait()
+
+    def RemoveWait(self, square):
         self.remove(square)
         self.wait()
+
+    def AddWait(self, square):
+        self.add(square)
+        self.wait()
+        return square
 
 with tempconfig({"quality": "high_quality", "preview": True}):
     scene = AddWaitRemove()
