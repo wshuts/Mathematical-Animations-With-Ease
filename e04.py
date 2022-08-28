fb906a7c2df78550fe7db78dc95397ea2845141d
wshuts <shutswilliam@gmail.com>
2022-08-27 21:26:09 -0500

    Example: AllUpdaterTypes tip: use disable caching flag when editing updaters

--- e04.py
+++ e04.py
@@ -11,3 +11,24 @@ class FirstExample(Scene):
         self.play(blue_dot.animate.shift(RIGHT))
         self.play(blue_dot.animate.scale(3))
         self.play(blue_dot.animate.center())
+
+class AllUpdaterTypes(Scene):
+    def construct(self):
+        red_dot = Dot(color=RED).shift(LEFT)
+        pointer = Arrow(ORIGIN, RIGHT).next_to(red_dot, LEFT)
+        pointer.add_updater(
+            lambda mobject: mobject.next_to(red_dot, LEFT)
+        )
+
+        def shifter(mob, dt):
+            mob.shift(2*dt*RIGHT)
+        red_dot.add_updater(shifter)
+
+        def scene_scaler(dt):
+            for mob in self.mobjects:
+                mob.set(width=2/(1 + np.linalg.norm(mob.get_center())))
+        self.add_updater(scene_scaler)
+
+        self.add(red_dot, pointer)
+        self.update_self(0)
+        self.wait(5)
